29b69b1257a8baf0cadfa84de1cb220558c2e859
Antti Kivi <antti@anttikivi.fi>
2024-09-08 17:10:37 +0300

    feat: add more stylish page titles

--- views/partials/navigation.php
+++ views/partials/navigation.php
@@ -1,9 +1,15 @@
 <header class="w-full">
-    <div class="container">
+    <div class="container my-6">
         <?php if ($is_home) : ?>
-        <h1 class="font-bold text-3xl text-center">Libri</h1>
+        <h1 class="text-center md:space-y-3">
+            <span class="block font-bold text-3xl md:text-5xl">Libri</span>
+            <span class="block text-xl md:text-2xl">Susannae Anttique</span>
+        </h1>
         <?php else : ?>
-        <p>Libri</p>
+        <p class="text-center md:space-y-2">
+            <span class="block font-bold text-2xl md:text-4xl">Libri</span>
+            <span class="block text-md md:text-xl">Susannae Anttique</span>
+        </p>
         <?php endif ?>
     </div>
 </header>
