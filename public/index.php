7cfd2e8aa8e4f2d32d55626579ebdf795d589cb8
Antti Kivi <antti@anttikivi.fi>
2024-09-08 14:27:39 +0300

    build: add an npm script for formatting

--- public/index.php
+++ public/index.php
@@ -8,9 +8,7 @@ const BASE_PATH = __DIR__ . '/../';
 session_start();
 
 require BASE_PATH . 'vendor/autoload.php';
-
 require BASE_PATH . 'functions.php';
-
 require BASE_PATH . 'bootstrap.php';
 
 $router = new Router();
