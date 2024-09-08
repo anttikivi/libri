ec9571aceb04ee6b02c3554f2f24d7491047d544
Antti Kivi <antti@anttikivi.fi>
2024-09-08 12:55:03 +0300

    feat: add placeholder header and footer to the views

--- views/index.view.php
+++ views/index.view.php
@@ -1,1 +1,5 @@
+<?php require('partials/head.php') ?>
+
 <p>Hello, world!</p>
+
+<?php require('partials/footer.php') ?>
---
+++ views/partials/head.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="fi">
+    <head>
+        <meta charset="UTF-8">
+        <title>Libri</title>
+        <link href="/app.css" rel="stylesheet">
+    </head>
+
+    <body class="h-full">
